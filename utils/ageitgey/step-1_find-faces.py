f65e3662606b5136a9b54d8ef538b1e1db273db0
Xiaobin Zhang (xiaobizh) <xiaobizh@cisco.com>
2018-07-30 14:38:25 +0800

    code clean

--- utils/ageitgey/step-1_find-faces.py
+++ utils/ageitgey/step-1_find-faces.py
@@ -13,16 +13,16 @@ MODEL='hog'
 def main(args):
     # Take the image file name from the command line
     file_name = args.image_file
-    
+
     # Create a HOG face detector using the built-in dlib class
     face_detector = dlib.get_frontal_face_detector()
     cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_face_detector_model)
-    
+
     win = dlib.image_window()
-    
+
     # Load the image into an array
     image = io.imread(file_name)
-    
+
     # Run the face detector on the image data.
     # The result will be the bounding boxes of the faces in our image.
     if MODEL == 'hog':
@@ -32,22 +32,22 @@ def main(args):
         detected_faces = [face.rect for face in detected_faces]
     else:
         raise ValueError("Invalid detector model type. Supported models are ['hog', 'cnn'].")
-    
+
     print("I found {} faces in the file {}".format(len(detected_faces), file_name))
-    
+
     # Open a window on the desktop showing the image
     win.set_image(image)
-    
+
     # Loop through each face we found in the image
     for i, face_rect in enumerate(detected_faces):
-        # Detected faces are returned as an object with the coordinates 
+        # Detected faces are returned as an object with the coordinates
         # of the top, left, right and bottom edges
         print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
-    
+
         # Draw a box around each face we found
         win.add_overlay(face_rect)
-    	        
-    # Wait until the user hits <enter> to close the window	        
+
+    # Wait until the user hits <enter> to close the window
     dlib.hit_enter_to_continue()
 
 
--- utils/ageitgey/step-3_get-faces-encoding.py
+++ utils/ageitgey/step-3_get-faces-encoding.py
@@ -17,50 +17,50 @@ MODEL='cnn'
 def main(args):
     # Take the image file name from the command line
     file_name = args.image_file
-    
+
     # Create a HOG face detector using the built-in dlib class
     face_detector = dlib.get_frontal_face_detector()
     face_pose_predictor = dlib.shape_predictor(predictor_model)
     face_encoder = dlib.face_recognition_model_v1(face_recognition_model)
-    
+
     win = dlib.image_window()
-    
+
     # Take the image file name from the command line
     file_name = sys.argv[1]
-    
+
     # Load the image
     image = io.imread(file_name)
-    
+
     # Run the HOG face detector on the image data
     detected_faces = face_detector(image, 1)
-    
+
     print("Found {} faces in the image file {}".format(len(detected_faces), file_name))
-    
+
     # Show the desktop window with the image
     win.set_image(image)
-    
+
     # Loop through each face we found in the image
     for i, face_rect in enumerate(detected_faces):
-    
-        # Detected faces are returned as an object with the coordinates 
+
+        # Detected faces are returned as an object with the coordinates
         # of the top, left, right and bottom edges
         print("* Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()))
-    
+
         # Draw a box around each face we found
         win.add_overlay(face_rect)
-    
-    
+
+
         # Get the the face's pose
         pose_landmarks = face_pose_predictor(image, face_rect)
-    
+
         # Draw the face landmarks on the screen.
         win.add_overlay(pose_landmarks)
-    
+
         # Get the the face's encoding and convert to numpy array
         face_encoding = face_encoder.compute_face_descriptor(image, pose_landmarks, num_jitters=1)
         face_encoding = np.array(face_encoding)
         print("* Face #{}'s encoding:\n{}".format(i, face_encoding))
-    	        
+
     dlib.hit_enter_to_continue()
 
 
