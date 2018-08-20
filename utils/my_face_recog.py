11bbce6cdc6ac65f6b2d3c4e717052d04b99287e
Xiaobin Zhang <david8862@gmail.com>
2018-08-20 20:43:12 +0800

    util optimize

--- utils/my_face_recog.py
+++ utils/my_face_recog.py
@@ -60,19 +60,15 @@ def face_recog():
         # Grab a single frame of video
         ret, frame = video_capture.read()
         # Resize frame of video to 1/4 size for faster face recognition processing
-        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        #rgb_small_frame = small_frame[:, :, ::-1]
-        rgb_small_frame = small_frame
+        #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
         # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(rgb_small_frame, model='hog')
-            #face_locations = face_recognition.face_locations(rgb_small_frame, model='cnn')
-            face_landmarks_list = face_recognition.face_landmarks(rgb_small_frame)
-            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations, num_jitters=NUM_JITTERS)
+            face_locations = face_recognition.face_locations(frame, model='hog')
+            #face_locations = face_recognition.face_locations(frame, model='cnn')
+            face_landmarks_list = face_recognition.face_landmarks(frame)
+            face_encodings = face_recognition.face_encodings(frame, face_locations, num_jitters=NUM_JITTERS)
 
             face_names = []
             for face_encoding in face_encodings:
@@ -92,10 +88,10 @@ def face_recog():
         # Display the results
         for (top, right, bottom, left), name in zip(face_locations, face_names):
             # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-            top *= 4
-            right *= 4
-            bottom *= 4
-            left *= 4
+            #top *= 4
+            #right *= 4
+            #bottom *= 4
+            #left *= 4
 
             # Draw a box around the face
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
